0b7184a5f526115081aebbb3c7791d2e39dbe958
zhenyu.yang <zhenyu.yang@dji.com>
2020-12-28 00:14:42 -0800

    added sample code for UDP broadcasting

---
+++ network/udp_broadcast/receiver.cpp
@@ -0,0 +1,78 @@
+
+// receiver 
+#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+	setvbuf(stdout, NULL, _IONBF, 0); 
+	fflush(stdout); 
+ 
+    // binding address
+	struct sockaddr_in addrto;
+	bzero(&addrto, sizeof(struct sockaddr_in));
+	addrto.sin_family = AF_INET;
+	addrto.sin_addr.s_addr = htonl(INADDR_ANY);
+	addrto.sin_port = htons(6000);
+	
+    // broadcasting address
+	struct sockaddr_in from;
+	bzero(&from, sizeof(struct sockaddr_in));
+	from.sin_family = AF_INET;
+	from.sin_addr.s_addr = htonl(INADDR_ANY);
+	from.sin_port = htons(6000);
+	
+	int sock = -1;
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) 
+	{   
+		cout<<"socket error"<<endl;	
+		return false;
+	}   
+ 
+	const int opt = 1;
+    //setting type to broadcast
+	int nb = 0;
+	nb = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char *)&opt, sizeof(opt));
+	if(nb == -1)
+	{
+		cout<<"set socket error..."<<endl;
+		return false;
+	}
+ 
+	if(bind(sock,(struct sockaddr *)&(addrto), sizeof(struct sockaddr_in)) == -1) 
+	{   
+		cout<<"bind error..."<<endl;
+		return false;
+	}
+ 
+	int len = sizeof(sockaddr_in);
+	char smsg[100] = {0};
+ 
+	while(1)
+	{
+        //receive message from broadcast address
+		int ret=recvfrom(sock, smsg, 100, 0, (struct sockaddr*)&from,(socklen_t*)&len);
+		if(ret<=0)
+		{
+			cout<<"read error...."<<sock<<endl;
+		}
+		else
+		{		
+			printf("%s\t", smsg);	
+		}
+ 
+		sleep(1);
+	}
+ 
+	return 0;
+}

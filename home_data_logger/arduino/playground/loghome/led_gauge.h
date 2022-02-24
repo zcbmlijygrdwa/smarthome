2021874c8c6c0042359da5cdee4ad54f940c867b
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2022-02-24 10:53:59 -0800

    updated temperature gauge

--- home_data_logger/arduino/playground/loghome/led_gauge.h
+++ home_data_logger/arduino/playground/loghome/led_gauge.h
@@ -35,4 +35,49 @@ void setLedGauge(double value, double min_val, double increamental)
     }
 }
 
+void setLedGaugeTwoDigits(double value)
+{
+    int led_num = 10;
+
+    int value_int = value;
+    int tens = value_int/10;
+    int digits = value_int%10;
+
+    int greens = digits/2;
+    int blues = digits-greens*2;
+
+    tens = min(tens, 5);
+
+    //Serial.print("value_int = ");
+    //Serial.print(value_int);
+    //Serial.print(", tens = ");
+    //Serial.print(tens);
+    //Serial.print(", digits = ");
+    //Serial.print(digits);
+    //Serial.print(", greens = ");
+    //Serial.print(greens);
+    //Serial.print(", blues = ");
+    //Serial.println(blues);
+
+    for(int i = 5 ; i < 10 ; i++)
+    {
+        if(10-i<=tens)
+            CircuitPlayground.setPixelColor(i, 255, 0 ,0);
+        else
+            CircuitPlayground.setPixelColor(i, 0, 0 ,0);
+    }
+    
+    //draw blue
+    for(int i = 0 ; i < 5 ; i++)
+    {
+        if(i<greens)
+                CircuitPlayground.setPixelColor(i, 0, 255 ,0);
+        else if(blues > 0 && i<greens+blues)
+                CircuitPlayground.setPixelColor(i, 0, 0 ,255);
+        else
+            CircuitPlayground.setPixelColor(i, 0, 0 ,0);
+    }
+
+}
+
 #endif
